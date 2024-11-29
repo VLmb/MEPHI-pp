318d4a4a9a79c6bdc39a788966cd40ab4e816f39
WocherZ <cay108@yandex.ru>
2024-11-29 21:32:18 +0300

    add neuro part and neuro module

--- TelegramBot/TeleBot.py
+++ TelegramBot/TeleBot.py
@@ -3,8 +3,29 @@ import sqlite3
 from telebot import types
 from telebot.apihelper import send_message
 
+from module import func
+
+
+
 bot = telebot.TeleBot('7691297714:AAG6Z9I7WXtbnqvGOGI1h3Qzfgh_d_70fnk')
 name = ''
+
+state = {}
+
+state = {
+    123434324: {
+        'language': 'Python',
+        'level': 'Senior',
+
+        ''
+    }
+
+}
+state[user_id] = {}
+state[user_id]['language'] = chosen_language
+state[user_id]['level'] = chosen_level
+state[user_id]['language'] =
+
 
 
 @bot.message_handler(commands=['start'])
