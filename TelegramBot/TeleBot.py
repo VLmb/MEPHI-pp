d646db240461e558b888c35e2dbb09af1d961c2b
VLmb <vlamb0809@gmail.com>
2024-11-30 14:19:18 +0300

    fin bot

--- TelegramBot/TeleBot.py
+++ TelegramBot/TeleBot.py
@@ -1,84 +1,62 @@
 import telebot
-import sqlite3
 from telebot import types
 from telebot.apihelper import send_message
-
-# from module import func
-
-
-
 bot = telebot.TeleBot('7691297714:AAG6Z9I7WXtbnqvGOGI1h3Qzfgh_d_70fnk')
-name = ''
 
-# state = {}
-#
-# state = {
-#     123434324: {
-#         'language': 'Python',
-#         'level': 'Senior',
-#         'suggestions': '...'
-#     }
-# }
-# state[user_id] = {}
-# state[user_id]['language'] = chosen_language
-# state[user_id]['level'] = chosen_level
-# state[user_id]['language'] =
+language = ''
+level = ''
+wishes = ''
+@bot.message_handler(content_types=['text'])
+def start(message):
 
+    markup = types.ReplyKeyboardMarkup()
+    bt1 = types.KeyboardButton('python')
+    bt2 = types.KeyboardButton('java')
+    bt3 = types.KeyboardButton('C++')
+    markup.row(bt1, bt2, bt3)
 
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Какой язык ты бы хотел изучить?", reply_markup=markup)
+        bot.register_next_step_handler(message, get_language) #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.from_user.id, 'Привет, я бот который помогает айти сотрудникам улучшать свои навыки! Если ты хочешь стать лучше нужно составить твою анкету, для этого напиши  /reg')
 
-@bot.message_handler(commands=['start'])
-def baza(mes_baz):
-    conn = sqlite3.connect('course.sql')
-    cur = conn.cursor()
+def get_language(message): #получаем фамилию
 
-    cur.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment primary key, name varchar(50), luguage varchar(50))')
-    conn.commit()
-    cur.close()
-    conn.close()
+    markup = types.ReplyKeyboardMarkup()
+    bt1 = types.KeyboardButton('Junior')
+    bt2 = types.KeyboardButton('Middle')
+    bt3 = types.KeyboardButton('Senior')
+    markup.row(bt1, bt2, bt3)
 
-    bot.send_message(mes_baz.chat.id, 'Привет сейчас начнем составлять твою анкету! Введите ваше имя')
-    bot.register_next_step_handler(mes_baz, user_name)
+    global language
+    language = message.text
+    bot.send_message(message.from_user.id, 'Выберете ваш уровень программирования!', reply_markup=markup)
+    bot.register_next_step_handler(message, get_level)
 
-def user_name(massage):
-    global name
-    name = massage.text.strip()
-    bot.send_message(massage.chat.id, 'введите язык')
-    bot.register_next_step_handler(massage, luguage)
+def get_level(message):
+    global level
+    level = message.text
+    bot.send_message(message.from_user.id,'Теперь напиши свои пожелания по обучению!')
+    bot.register_next_step_handler(message, get_wishes)
 
-def luguage(message):
-    yaz = message.text.strip()
+def get_wishes(message):
 
-    conn = sqlite3.connect('course.sql')
-    cur = conn.cursor()
+    markup = types.ReplyKeyboardMarkup()
+    bt1 = types.KeyboardButton('Да')
+    bt2 = types.KeyboardButton('Нет')
+    markup.row(bt1, bt2)
 
-    cur.execute('INSERT INTO users (name, luguage) VALUES ("%s","%s")' % (name, yaz))
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
-    markup = types.InlineKeyboardMarkup()
-    bt1 = types.InlineKeyboardButton('Посмотреть анкету', callback_data='users')
-    markup.row(bt1)
-    bot.send_message(message.chat.id,'Ваша анкета готова!',reply_markup=markup)
-
-@bot.callback_query_handler(func=lambda callback: True)
-def data(call):
-    conn = sqlite3.connect('course.sql')
-    cur = conn.cursor()
-
-    cur.execute('SELECT * FROM users')
-    users = cur.fetchall()
-
-    info= ''
-    for el in users:
-        info+=f'имя: {el[1]}\n Язык:{el[2]}\n'
-
-    cur.close()
-    conn.close()
-
-    bot.send_message(call.message.chat.id, info)
+    global wishes
+    wishes = message.text
+    bot.send_message(message.from_user.id, 'Вот твоя анкета\n Язык программирования: ' + language + '\n Уровень программирования: ' + level + '\n пожелания: '+wishes+'')
+    bot.send_message(message.from_user.id, 'Все верно?', reply_markup=markup)
+    bot.register_next_step_handler(message, get_feedback)
 
 
-bot.polling(none_stop=True)
+def get_feedback(message):
+    if message.text == 'Да':
+        bot.send_message(message.from_user.id, "Тогда сейчас вышлю тебе несколько курсов")
+    else:
+        bot.send_message(message.from_user.id, "Чтобы ты хотел изменить?")
+
