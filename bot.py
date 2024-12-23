16330d3777eda8aa645e912be85f1576804c716b
VLmb <vlamb0809@gmail.com>
2024-12-23 14:15:11 +0300

    Upadate bot

--- bot.py
+++ bot.py
@@ -29,22 +29,30 @@ def start(message):
             'Если ты хочешь стать лучше, напиши /recommendation для составления анкеты.',
         )
 
-
+@bot.message_handler(content_types=['text'])
 def get_language(message):
-    user_data['language'] = message.text
+    if message.text == 'Python' or message.text == 'Java' or message.text == 'C++':
+        user_data['language'] = message.text
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    buttons = ['Junior', 'Middle', 'Senior']
-    markup.add(*[types.KeyboardButton(text) for text in buttons])
-    bot.send_message(message.from_user.id, 'Выберите ваш уровень программирования!', reply_markup=markup)
-    bot.register_next_step_handler(message, get_level)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        buttons = ['Junior', 'Middle', 'Senior']
+        markup.add(*[types.KeyboardButton(text) for text in buttons])
+        bot.send_message(message.from_user.id, 'Выберите ваш уровень программирования!', reply_markup=markup)
+        bot.register_next_step_handler(message, get_level)
+    else:
+        bot.send_message(message.from_user.id, 'Выберете ваш язык программирования, нажав на встроенную кнопку')
+        bot.register_next_step_handler(message, get_language)
 
-
+@bot.message_handler(content_types=['text'])
 def get_level(message):
-    user_data['level'] = message.text
+    if message.text == 'Junior' or message.text == 'Middle' or message.text == 'Senior':
+        user_data['level'] = message.text
 
-    bot.send_message(message.from_user.id, 'Теперь напишите свои пожелания по обучению!')
-    bot.register_next_step_handler(message, get_wishes)
+        bot.send_message(message.from_user.id, 'Теперь напишите свои пожелания по обучению!')
+        bot.register_next_step_handler(message, get_wishes)
+    else:
+        bot.send_message(message.from_user.id, 'Выберете ваш уровень программирования, нажав на встроенную кнопку')
+        bot.register_next_step_handler(message, get_level)
 
 
 def get_wishes(message):
@@ -70,9 +78,8 @@ def get_feedback(message):
         recommended_courses = get_course_recommendations(user_data)
         bot_recommendation_message = format_course_response(recommended_courses)
         bot.send_message(message.from_user.id, bot_recommendation_message, parse_mode='HTML')
-    else:
-        bot.send_message(message.from_user.id, "Что бы ты хотел изменить?")
-
+    if message.text == 'Нет':
+        bot.send_message(message.from_user.id, 'Тогда давай заполним анкету заново! Напиши /recommendation')
 
 if __name__ == '__main__':
     bot.polling(none_stop=False, interval=0)
